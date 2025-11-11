205ba2ee0b3d1dc4f2b798fefb1d7ac4e8dd5ea6
Peter <zpetiko000@gmail.com>
2025-11-11 21:50:40 +0100

    Testing MPC, saving logsout (logsoutInfo.m description).

---
+++ mpc/logsoutInfo.m
@@ -0,0 +1,29 @@
+% mpc100Fs1000MV01W010KFNoise0.mat
+% mpc100Fs1000MV005W0100KFNoise0.mat
+% mpc100Fs1000MV0001W0500KFNoise0.mat
+% With p = 12, m = 10 horizon.
+
+% mpc100Fs1000MV0001W0500KFNoise0p20m2.mat
+% With p = 20, m = 2.
+
+% mpc50Fs1000MV01W050KFNoise0im20.mat
+% With p = 12, m = 10 horizon. i_m = 20.
+
+% mpc50Fs1000MV01W050KFNoise0p30m3.mat
+% i_m = 20.
+
+% mpc100Fs1000MV01W050KFNoise0p30m3.mat
+% With p = 12, m = 10 horizon (neveben veletlen maradt).
+% i_m = 20.
+
+% mpc100Fs1000MV01W050KFNoise0im10.mat
+% i_m = 20.
+
+% mpc100Fs1000MV01W050KFNoise0p50m3im10.mat
+% With p = 50, m = 3.
+% i_m = 10.
+
+% mpc100Fs1000MV01W01KFNoise0p50m5im10.mat
+% With p = 50, m = 5.
+% i_m = 10.
+% Weights = [0 0 1 0 0 0];
