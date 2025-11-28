2b84d9ee0689ff6e8aa82403bb80e41bf8fa6da3
Peter <zpetiko000@gmail.com>
2025-11-28 11:14:25 +0100

    Physical unit conversion to sensors. Adding logs, tuning position control PID:

--- lqrTest.m
+++ lqrTest.m
@@ -11,3 +11,16 @@ R_aug_lqr_temp = 15;
 % LQR Controller - for the augmented system                     
 [K, ~, ~] = dlqr(Ad_aug, Bd_aug, Q_aug_lqr_temp, R_aug_lqr_temp);
 K = single(K)
+
+% Velocity angle PID
+Tw_vel = 1;
+Kp_vel = 0.01;
+Ki_vel = 0.01; 
+Kd_vel = 0;
+
+% Position angle PID
+targetPosition = 0.1;
+Tw_pos = 1;
+Kp_pos = 0.1;
+Ki_pos = 0.0001; 
+Kd_pos = 0.1;
